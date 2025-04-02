d35d059c9241aa8f2bc157e2833668245c0052a9
Vladimir <smirnov.vladimir@panoramikltd.com>
2025-04-02 11:21:54 +0300

    build v0.1

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Tchiff-Wedding-0.1";
+const contentToCache = [
+    "Build/Wedding.loader.js",
+    "Build/Wedding.framework.js.br",
+    "Build/Wedding.data.br",
+    "Build/Wedding.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
